1f6973142662012f35330e1187da3a4ea48ae9d6
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2019-12-07 16:14:06 +0900

    first commit

---
+++ src/main/java/com/example/EcRamen2019Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcRamen2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcRamen2019Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcRamen2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcRamen2019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
